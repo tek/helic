b31741476784e058f9960c681510c6f50a0510cb
tek <2632+tek@users.noreply.github.com>
2025-03-20 14:35:10 +0000

    Bump 37 dependencies
    New versions:
    
    * base-4.17.2.1 [>=4.17.2.1 && <4.21]
    * chronos-1.1.5 [>=1.1.5 && <1.2]
    * containers-0.6.7 [>=0.6.7 && <0.8]
    * exon-1.7.0.0 [>=1.7.0.0 && <1.8]
    * fast-logger-3.2.1 [>=3.2.1 && <3.3]
    * gi-gdk-3.0.29 [>=3.0.29 && <3.1]
    * gi-glib-2.0.30 [>=2.0.30 && <2.1]
    * gi-gtk-3.0.43 [>=3.0.43 && <3.1]
    * hostname-1.0 [>=1.0 && <1.1]
    * network-3.1.4.0 [>=3.1.4.0 && <3.2]
    * optparse-applicative-0.18.1.0 [>=0.18.1.0 && <0.19]
    * path-0.9.5 [>=0.9.5 && <0.10]
    * path-io-1.8.2 [>=1.8.2 && <1.9]
    * polysemy-1.9.2.0 [>=1.9.2.0 && <1.10]
    * polysemy-chronos-0.7.0.0 [>=0.7.0.0 && <0.8]
    * polysemy-conc-0.14.1.1 [>=0.14.1.1 && <0.15]
    * polysemy-http-0.13.1.0 [>=0.13.1.0 && <0.14]
    * polysemy-log-0.11.0.0 [>=0.11.0.0 && <0.12]
    * polysemy-plugin-0.4.4.0 [>=0.4.4.0 && <0.5]
    * polysemy-process-0.14.1.0 [>=0.14.1.0 && <0.15]
    * polysemy-test-0.10.0.1 [>=0.10.0.1 && <0.11]
    * polysemy-time-0.7.0.0 [>=0.7.0.0 && <0.8]
    * prelate-0.8.0.0 [>=0.8.0.0 && <0.9]
    * random-1.2.1.2 [>=1.2.1.2 && <1.3]
    * servant-0.20.2 [>=0.20.2 && <0.21]
    * servant-client-0.20.2 [>=0.20.2 && <0.21]
    * servant-server-0.20.2 [>=0.20.2 && <0.21]
    * table-layout-1.0.0.1 [>=1.0.0.1 && <1.1]
    * tasty-1.4.3 [>=1.4.3 && <1.6]
    * terminal-size-0.3.4 [>=0.3.4 && <0.4]
    * torsor-0.1 [>=0.1 && <0.2]
    * transformers-0.5.6.2 [>=0.5.6.2 && <0.7]
    * typed-process-0.2.12.0 [>=0.2.12.0 && <0.3]
    * wai-extra-3.1.13.0 [>=3.1.13.0 && <3.2]
    * warp-3.4.0 [>=3.4.0 && <3.5]
    * yaml-0.11.11.0 [>=0.11.11.0 && <0.12]
    * zeugma-0.10.0.0 [>=0.10.0.0 && <0.11]

--- ops/managed.nix
+++ ops/managed.nix
@@ -2,151 +2,151 @@
   bounds = {
     helic = {
       base = {
-        lower = null;
+        lower = "4.17.2.1";
         upper = "4.21";
       };
       chronos = {
-        lower = null;
+        lower = "1.1.5";
         upper = "1.2";
       };
       containers = {
-        lower = null;
+        lower = "0.6.7";
         upper = "0.8";
       };
       exon = {
-        lower = null;
+        lower = "1.7.0.0";
         upper = "1.8";
       };
       fast-logger = {
-        lower = null;
+        lower = "3.2.1";
         upper = "3.3";
       };
       gi-gdk = {
-        lower = null;
+        lower = "3.0.29";
         upper = "3.1";
       };
       gi-glib = {
-        lower = null;
+        lower = "2.0.30";
         upper = "2.1";
       };
       gi-gtk = {
-        lower = null;
+        lower = "3.0.43";
         upper = "3.1";
       };
       hostname = {
-        lower = null;
+        lower = "1.0";
         upper = "1.1";
       };
       network = {
-        lower = null;
+        lower = "3.1.4.0";
         upper = "3.2";
       };
       optparse-applicative = {
-        lower = null;
+        lower = "0.18.1.0";
         upper = "0.19";
       };
       path = {
-        lower = null;
+        lower = "0.9.5";
         upper = "0.10";
       };
       path-io = {
-        lower = null;
+        lower = "1.8.2";
         upper = "1.9";
       };
       polysemy = {
-        lower = null;
+        lower = "1.9.2.0";
         upper = "1.10";
       };
       polysemy-chronos = {
-        lower = null;
+        lower = "0.7.0.0";
         upper = "0.8";
       };
       polysemy-conc = {
-        lower = null;
+        lower = "0.14.1.1";
         upper = "0.15";
       };
       polysemy-http = {
-        lower = null;
+        lower = "0.13.1.0";
         upper = "0.14";
       };
       polysemy-log = {
-        lower = null;
+        lower = "0.11.0.0";
         upper = "0.12";
       };
       polysemy-plugin = {
-        lower = null;
+        lower = "0.4.4.0";
         upper = "0.5";
       };
       polysemy-process = {
-        lower = null;
+        lower = "0.14.1.0";
         upper = "0.15";
       };
       polysemy-test = {
-        lower = null;
+        lower = "0.10.0.1";
         upper = "0.11";
       };
       polysemy-time = {
-        lower = null;
+        lower = "0.7.0.0";
         upper = "0.8";
       };
       prelate = {
-        lower = null;
+        lower = "0.8.0.0";
         upper = "0.9";
       };
       random = {
-        lower = null;
+        lower = "1.2.1.2";
         upper = "1.3";
       };
       servant = {
-        lower = null;
+        lower = "0.20.2";
         upper = "0.21";
       };
       servant-client = {
-        lower = null;
+        lower = "0.20.2";
         upper = "0.21";
       };
       servant-server = {
-        lower = null;
+        lower = "0.20.2";
         upper = "0.21";
       };
       table-layout = {
-        lower = null;
+        lower = "1.0.0.1";
         upper = "1.1";
       };
       tasty = {
-        lower = null;
+        lower = "1.4.3";
         upper = "1.6";
       };
       terminal-size = {
-        lower = null;
+        lower = "0.3.4";
         upper = "0.4";
       };
       torsor = {
-        lower = null;
+        lower = "0.1";
         upper = "0.2";
       };
       transformers = {
-        lower = null;
+        lower = "0.5.6.2";
         upper = "0.7";
       };
       typed-process = {
-        lower = null;
+        lower = "0.2.12.0";
         upper = "0.3";
       };
       wai-extra = {
-        lower = null;
+        lower = "3.1.13.0";
         upper = "3.2";
       };
       warp = {
-        lower = null;
+        lower = "3.4.0";
         upper = "3.5";
       };
       yaml = {
-        lower = null;
+        lower = "0.11.11.0";
         upper = "0.12";
       };
       zeugma = {
-        lower = null;
+        lower = "0.10.0.0";
         upper = "0.11";
       };
     };
@@ -191,9 +191,86 @@
       yaml = "0.11.11.2";
       zeugma = "0.10.0.1";
     };
+    lower = {
+      base = "4.17.2.1";
+      chronos = "1.1.5";
+      containers = "0.6.7";
+      exon = "1.7.0.0";
+      fast-logger = "3.2.1";
+      gi-gdk = "3.0.29";
+      gi-glib = "2.0.30";
+      gi-gtk = "3.0.43";
+      hostname = "1.0";
+      network = "3.1.4.0";
+      optparse-applicative = "0.18.1.0";
+      path = "0.9.5";
+      path-io = "1.8.2";
+      polysemy = "1.9.2.0";
+      polysemy-chronos = "0.7.0.0";
+      polysemy-conc = "0.14.1.1";
+      polysemy-http = "0.13.1.0";
+      polysemy-log = "0.11.0.0";
+      polysemy-plugin = "0.4.4.0";
+      polysemy-process = "0.14.1.0";
+      polysemy-test = "0.10.0.1";
+      polysemy-time = "0.7.0.0";
+      prelate = "0.8.0.0";
+      random = "1.2.1.2";
+      servant = "0.20.2";
+      servant-client = "0.20.2";
+      servant-server = "0.20.2";
+      table-layout = "1.0.0.1";
+      tasty = "1.4.3";
+      terminal-size = "0.3.4";
+      torsor = "0.1";
+      transformers = "0.5.6.2";
+      typed-process = "0.2.12.0";
+      wai-extra = "3.1.13.0";
+      warp = "3.4.0";
+      yaml = "0.11.11.0";
+      zeugma = "0.10.0.0";
+    };
   };
   initial = {
     latest = {};
+    lower = {
+      chronos = "1.1.5";
+      containers = "0.6.7";
+      exon = "1.7.0.0";
+      fast-logger = "3.2.1";
+      gi-gdk = "3.0.29";
+      gi-glib = "2.0.30";
+      gi-gtk = "3.0.43";
+      hostname = "1.0";
+      network = "3.1.4.0";
+      optparse-applicative = "0.18.1.0";
+      path = "0.9.5";
+      path-io = "1.8.2";
+      polysemy = "1.9.2.0";
+      polysemy-chronos = "0.7.0.0";
+      polysemy-conc = "0.14.1.1";
+      polysemy-http = "0.13.1.0";
+      polysemy-log = "0.11.0.0";
+      polysemy-plugin = "0.4.4.0";
+      polysemy-process = "0.14.1.0";
+      polysemy-test = "0.10.0.1";
+      polysemy-time = "0.7.0.0";
+      prelate = "0.8.0.0";
+      random = "1.2.1.2";
+      servant = "0.20.2";
+      servant-client = "0.20.2";
+      servant-server = "0.20.2";
+      table-layout = "1.0.0.1";
+      tasty = "1.4.3";
+      terminal-size = "0.3.4";
+      torsor = "0.1";
+      transformers = "0.5.6.2";
+      typed-process = "0.2.12.0";
+      wai-extra = "3.1.13.0";
+      warp = "3.4.0";
+      yaml = "0.11.11.0";
+      zeugma = "0.10.0.0";
+    };
   };
   overrides = {
     latest = {
@@ -292,6 +369,108 @@
       zeugma = {
         version = "0.10.0.1";
         hash = "14k0lq3ghanvxw47g43vvzfw4d9cm04bmc2fn5cp4y3vslflaknj";
+      };
+    };
+    lower = {
+      cabal-doctest = {
+        version = "1.0.11";
+        hash = "152rqpicqpvigjpy4rf1kjlwny1c7ys1r0r123wdjafvv1igflii";
+      };
+      chronos = {
+        version = "1.1.5";
+        hash = "1rja3hsf7s4gfm1xq5s2gydx9d0jckcjpza9rqvhbpv1h0qsh3f3";
+      };
+      exon = {
+        version = "1.7.0.0";
+        hash = "142i8ka6b16ydllhhb2305ml3hij66h6y555fp6cvc82166kdrhb";
+      };
+      fast-logger = {
+        version = "3.2.1";
+        hash = "0cgipf7zfmxjm2j52wqkfxd4k1m5clq6n8z3nsmscp9d18prgvx8";
+      };
+      http-semantics = {
+        version = "0.0.0";
+        hash = "0jdlakynqm1vm22kb1g1g1mh7h0ihk7mx8l2zzvdaqmqn10lwmh4";
+      };
+      http2 = {
+        version = "5.2.0";
+        hash = "12zrgymnbsc9ybz1mz0a183vzrpawrsv9hmhhsnrbxqc2im6g52k";
+      };
+      incipit = {
+        version = "0.10.0.0";
+        hash = "1r3y2wp8wz1ii28a6wb76z6w3sgiah158kwsadrr13w6iryhq047";
+      };
+      network-control = {
+        version = "0.1.4";
+        hash = "00kb50jj14661p0bfb2hgzrq93z9rjch082hp6wda65hvgxfz289";
+      };
+      polysemy-chronos = {
+        version = "0.7.0.0";
+        hash = "0srq4xda9rracrf0frqh9vb00fscxyjv8w6fgavgxbxsrir856i8";
+      };
+      polysemy-conc = {
+        version = "0.14.1.1";
+        hash = "1xli6ja9f7qx2k9956lw4h9y5ywdglhgw769afxw9d4w9avclx28";
+      };
+      polysemy-http = {
+        version = "0.13.1.0";
+        hash = "0ii0ldlr2j4mby6x9l04jxwnf06r71kb8smnqk2hwjhaapai37pq";
+      };
+      polysemy-log = {
+        version = "0.11.0.0";
+        hash = "1phzwj2ig0vx1anscg3qv53ysa0f7gks37pc8gfkh2aws3qp6cda";
+      };
+      polysemy-plugin = {
+        version = "0.4.4.0";
+        hash = "08ry72bw78fis9iallzw6wsrzxnlmayq2k2yy0j79hpw4sp8knmg";
+      };
+      polysemy-process = {
+        version = "0.14.1.0";
+        hash = "125fiwq30ybncmc0pb25ki3k2sxbhkjz4k2i53bcj9y026xgvjyi";
+      };
+      polysemy-test = {
+        version = "0.10.0.1";
+        hash = "1sp9iag1brknmdy0qvmgnmynwc4gbg1jy21w584x1m2hpqi25p6j";
+      };
+      polysemy-time = {
+        version = "0.7.0.0";
+        hash = "0imvjiybxrsggh72pfkd226pvzhz5hg1zvxyd72b91a3xz1vynmq";
+      };
+      prelate = {
+        version = "0.8.0.0";
+        hash = "0id72rbynmbb15ld8pv8nijll3k50x2mrpcqsv8dkbs7q05fn9vg";
+      };
+      servant-server = {
+        version = "0.20.2";
+        hash = "1xp86ha73fkqbsxyycr0wga0k106vfb4kpjyzh055l2qb47kyj9j";
+      };
+      torsor = {
+        version = "0.1";
+        hash = "07p05f7j6h99pxx1v4j0ya5py4nc93gnbj09pdhl23i2zc75064f";
+      };
+      wai-app-static = {
+        version = "3.1.9";
+        hash = "1kwvzy9w4v76q5bk4xwq7kz9q9l8867my6zvsv731x6xkhy7wr2c";
+      };
+      wai-extra = {
+        version = "3.1.13.0";
+        hash = "1niaix0vhfjw2ch65mhd4favzr6h53vyd9vlkv28skrgsdzicc0h";
+      };
+      wai-logger = {
+        version = "2.5.0";
+        hash = "1l0gspzbwkrs1dqay2rv5wz1gg49a31l3vyl741b2j2apkgamf3p";
+      };
+      warp = {
+        version = "3.4.0";
+        hash = "1iskx4vxb3f8b0qfqwxyljgv801sh9jw3kfc1vxxr0j0zlvy3j7r";
+      };
+      yaml = {
+        version = "0.11.11.0";
+        hash = "044dlhv6mx4r0s0xl73zkchl5n4ckq1x8hh72x6mn5g5q3nn189r";
+      };
+      zeugma = {
+        version = "0.10.0.0";
+        hash = "16rv92l40bhrpf4mdj3f39wqb483jgj3jzkgckanfy4z6nfglirm";
       };
     };
   };
